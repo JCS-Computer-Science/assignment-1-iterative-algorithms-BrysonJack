1a97135f3ead04b65ddd1eb9f7e5ba096e61d652
Bryson <Brysonj2@educbe.ca>
2022-09-19 11:25:33 -0600

    Added  Misc. Testing Files

---
+++ TESTING/index.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < i + 1; i++) {
+    console.log(i)
+    console.log(100 + 1)
+    if (i = 3) {
+        console.log ("final thing" + i)
+        break
+    }
+    console.log ("not seen i hope :)")
+    // Testing if "break" is for breaking loops (it is)
+}
