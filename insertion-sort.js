f9431f3c72a9a9e1d2a9a6498215645f06ec28c7
Bryson <Brysonj2@educbe.ca>
2022-09-19 11:24:32 -0600

    Finished Completion Sort

--- insertion-sort.js
+++ insertion-sort.js
@@ -4,8 +4,30 @@ function insertionSort(array) {
 	 ** before it over and over as long as the element before it
 	 ** is bigger
 	 */
+	// let Displace = 0
+	let SortStop = 4
+	let InsertSort = array
+	let InsertLength  = array.length;
+	if (InsertSort[0] > InsertSort[1]) {
+		let Store = InsertSort[0]
+		InsertSort[0] = InsertSort[1]
+		InsertSort[1] = Store
+	}
+	for (let i = 0; i < (InsertLength + 1); i++) {
+		if (InsertSort[i + 1] > InsertSort[i + 2]) {
+			for (let x = 0; x < SortStop; x++) {
+				if (InsertSort[x] > InsertSort[i + 2]) {
+					Store = InsertSort[x]
+					InsertSort[x] = InsertSort[i + 2]
+					InsertSort[i + 2] = Store
+				}
+			}
+			SortStop = SortStop + 1
+		}
 
-	return array;
+	}  
+
+	return InsertSort;
 }
 
 module.exports = insertionSort;
