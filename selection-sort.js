849afd8578d0cb7744ff1c1a8caf85b71fb0e556
Bryson <Brysonj2@educbe.ca>
2022-09-20 10:07:13 -0600

    Completed Selection Sort

--- selection-sort.js
+++ selection-sort.js
@@ -6,6 +6,24 @@ function selectionSort(array) {
 	 ** for the smallest, then swap it with the second element.
 	 ** Continue like this until you have only one element left.
 	 */
+	let SelectionArray = array
+	let SelectionLength = array.length
+	for (let i = 0; i < SelectionLength; i++) {
+		let Lowest = i
+		for (let x = i + 1; x < SelectionLength; x++) {
+			if (SelectionArray[x] < SelectionArray[Lowest]) {
+				Lowest = x
+			}
+		}
+		if (Lowest != i) {
+			Store = SelectionArray[i]
+			SelectionArray[i] = SelectionArray[Lowest]
+			SelectionArray[Lowest] = Store
+		}
+	}
+
+
+
 
 	return array;
 }
