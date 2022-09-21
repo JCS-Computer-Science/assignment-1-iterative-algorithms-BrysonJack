96d507ac368dbca26170a333581fa7ddd692d6a3
Bryson <Brysonj2@educbe.ca>
2022-09-21 10:48:34 -0600

    Completed Linear Search

--- linear-search.js
+++ linear-search.js
@@ -1,12 +1,21 @@
 function linearSearch(array, searchTerm) {
 	/*
-	 ** Check each element from start to finish
-	 ** until you find an element that matches the search term
-	 **
-	 ** Return the index of the found element
-	 */
-
-	return null;
+	** Check each element from start to finish
+	** until you find an element that matches the search term
+	**
+	** Return the index of the found element
+	*/
+	let SearchReturn = 110
+	let SearchArray = array
+    let SearchLength = array.length
+	for (let i = 0; i < SearchLength; i++) {
+		if(SearchArray[i] === searchTerm){
+			SearchReturn = i
+			// array.indexOf(SearchArray)
+		}
+		console.log("ran")
+	}
+	return SearchReturn;
 }
 
 module.exports = linearSearch;
